e1039bef67c8746dd2b96290349a65f1f376b311
afeiship <1290657123@qq.com>
2018-05-29 19:32:13 +0800

    issue: opmtize

--- src/components/react-ant-image-pop-view.js
+++ src/components/react-ant-image-pop-view.js
@@ -13,20 +13,22 @@ export default class extends Component {
     className: PropTypes.string,
     placement: PropTypes.string,
     size: PropTypes.array,
+    popSize: PropTypes.array,
     urls: PropTypes.array,
   };
 
   static defaultProps = {
     placement: "rightTop",
     size: ['100px'],
+    popSize: ['100px'],
     urls: []
   };
   /*===properties end===*/
 
   get contentView() {
-    const { urls } = this.props;
+    const { urls, size } = this.props;
     return (
-      <ReactFigure>
+      <ReactFigure style={{ width: size[0], height: size[1] || size[0] }}>
         <img src={ urls[1]} />
       </ReactFigure>
     )
